d2a64249ac2c1916a3c0419bc44465e05b61ee13
terenitup <terentaniuchi@gmail.com>
2018-09-20 00:58:01 -0600

    added portfolio component content and styling

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import madlibs from './madlibs.png';
+import birthdayCountdown from './birthdayCountdown.png';
 
 export default class Portfolio extends Component {
 //   constructor() {
@@ -8,7 +10,42 @@ export default class Portfolio extends Component {
   render() {
     return (
       <div className='portfolio'>
-        <div>Portfolio</div>
+        <div className='madlibs'>
+          <div className='madlibs__text'>
+            Mad Libs App
+          </div>
+          
+          <div className='madlibs__thumbnail'>
+            <a className='madlibs__link' 
+              href='https://tt-madlibs.herokuapp.com/'
+              target='_blank'  
+            >
+              <img className='madlibs__img' 
+                src={madlibs} 
+                alt='Thumbnail of the madlibs app page' 
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className='birthdayCountdown'>
+          <div className='birthdayCountdown__text'>
+            Birthday Countdown App
+          </div>
+          
+          <div className='birthdayCountdown__thumbnail'>
+            <a className='birthdayCountdown__link' 
+              href='https://tt-birthday-countdown.herokuapp.com/'
+              target='_blank'  
+            >
+              <img className='birthdayCountdown__img' 
+                src={birthdayCountdown} 
+                alt='Thumbnail of the birthday countdown app page' 
+              />
+            </a>
+          </div>
+          
+        </div>
       </div>
       
     );

6b4845ccd8a9447bf444bfcb98c5c4d5e5deaa0c
terenitup <terentaniuchi@gmail.com>
2018-09-24 14:02:29 -0600

    added links to the contact page as well as some icons for each of the contact items

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -20,20 +20,64 @@ class Contact extends Component {
           <h1 className='contact__title'>CONTACT</h1>
           <div className='contact__paragraph'>
             <div className='contact__paragraphHeading'>
-              <div>phone</div>
-              <div>email</div>
-              <div>github</div>
-              <div>linkedin</div>
-              <div>instagram</div>
-              <div>facebook</div>
+              <div>
+                <i class="fas fa-mobile-alt"></i>
+                phone
+              </div>
+              <div><i class="far fa-envelope"></i>
+                email
+              </div>
+              <div><i class="fab fa-github"></i>
+                github
+              </div>
+              <div><i class="fab fa-linkedin-in"></i>
+                linkedin
+              </div>
+              <div><i class="fab fa-instagram"></i>
+                instagram
+              </div>
+              <div>
+                <br></br>
+                <br></br>
+                <i class="fab fa-facebook-f"></i>
+                facebook
+              </div>
             </div>
             <div className='contact__paragraphText'>
               <div>808.753.3247</div>
               <div>terentaniuchi@gmail.com</div>
-              <div>github.com/terenitup</div>
-              <div>linkedin.com/in/terentaniuchi</div>
-              <div>@teren_itup | @kei_designs | @foodlakecity</div>
-              <div>facebook.com/terent</div>
+              <div>
+                <a href="https://github.com/terenitup" 
+                  target="_blank">
+                  github.com/terenitup
+                </a>
+              </div>
+              <div>
+                <a href="https://linked.com/in/terentaniuchi"
+                  target="_blank">
+                  linkedin.com/in/terentaniuchi                
+                </a>
+              </div>
+              <div>
+                <a href="https://www.instagram.com/teren_itup/"
+                  target="_blank">
+                  @teren_itup
+                </a> 
+                <a href="https://www.instagram.com/kei_designs/"
+                  target="_blank"> 
+                  <br></br>@kei_designs
+                </a>
+                <a href="https://www.instagram.com/foodlakecity/"
+                  target="_blank"> 
+                  <br></br>@foodlakecity
+                </a>
+              </div>
+              <div>
+                <a href="https://www.facebook.com/terent"
+                  target="_blank">
+                  facebook.com/terent
+                </a>
+              </div>
             </div>
           </div>
         </div>
